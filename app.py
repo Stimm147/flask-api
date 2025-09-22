258e242623cffd88c32d72f2d2f0803614842c4b
stimm147 <michalczewski0@gmail.com>
2025-09-22 17:59:58 +0200

    test api v1

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import torch
+import torchvision.transforms as transforms
+from torchvision.models import mobilenet_v2
+from PIL import Image
+import requests
 
-# Inicjalizacja aplikacji Flask
 app = Flask(__name__)
-
-# --- KLUCZOWA KONFIGURACJA CORS ---
-# Zezwól na zapytania tylko z adresu URL Twojego frontendu.
-# To jest dokładnie ten sam mechanizm, który konfigurowałeś w Reflex.
 CORS(
     app,
     resources={
@@ -14,16 +14,35 @@ CORS(
     },
 )
 
-# Proste dane w pamięci, które będzie zwracać nasze API
-stats_data = {"page_visitors": 123, "projects_completed": 5, "api_status": "OK"}
+model = mobilenet_v2(pretrained=True)
+model.eval()
+
+transform = transforms.Compose(
+    [
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
 
 
-@app.route("/api/stats", methods=["GET"])
-def get_stats():
-    """Zwraca dane statystyczne w formacie JSON."""
-    return jsonify(stats_data)
+@app.route("/api/classify", methods=["POST"])
+def classify():
+    try:
+        url = request.json["url"]
+        image = Image.open(requests.get(url, stream=True).raw)
+
+        with torch.no_grad():
+            output = model(transform(image).unsqueeze(0))
+            prediction = torch.argmax(output).item()
+            confidence = torch.softmax(output, 1).max().item()
+
+        return jsonify(
+            {"class_id": prediction, "confidence": round(confidence * 100, 1)}
+        )
+    except:
+        return jsonify({"error": "Failed"}), 400
 
 
-# Ta część poniżej jest tylko do testów lokalnych, nie będzie używana na Railway
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
