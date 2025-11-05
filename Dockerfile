d00b0120fcbbcb9ff4cc3a6b8c2a2c5d620210e0
stimm147 <michalczewski0@gmail.com>
2025-11-05 11:41:18 +0100

    fastapi version

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Skopiuj resztę kodu aplikacji
 COPY . .
 
-# Uruchom aplikację za pomocą serwera produkcyjnego Gunicorn
+# Uruchom aplikację za pomocą Gunicorn z Uvicorn workers
 # Będzie nasłuchiwać na porcie podanym przez Railway w zmiennej $PORT
-CMD gunicorn --bind 0.0.0.0:${PORT} app:app
+CMD gunicorn -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:${PORT}
--- app.py
+++ app.py
@@ -1,50 +1,42 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 import torch
 import torchvision.transforms as transforms
-from torchvision.models import mobilenet_v2
+from torchvision.models import MobileNet_V3_Small_Weights, mobilenet_v3_small
 from PIL import Image
 import requests
 
-app = Flask(__name__)
-CORS(
-    app,
-    resources={
-        r"/api/*": {
-            "origins": "https://reflex-example-backend-production.up.railway.app"
-        }
-    },
-)
 
-model = mobilenet_v2(pretrained=True)
-model.eval()
+class ImageClassifier:
+    LABELS_URL = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
 
-transform = transforms.Compose(
-    [
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ]
-)
+    def __init__(self):
+        pass
 
+    def fetch_model(self):
+        model = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.DEFAULT)
+        model.eval()
+        return model
 
-@app.route("/api/classify", methods=["POST"])
-def classify():
-    try:
-        url = request.json["url"]
-        image = Image.open(requests.get(url, stream=True).raw)
-
-        with torch.no_grad():
-            output = model(transform(image).unsqueeze(0))
-            prediction = torch.argmax(output).item()
-            confidence = torch.softmax(output, 1).max().item()
-
-        return jsonify(
-            {"class_id": prediction, "confidence": round(confidence * 100, 1)}
+    def classify_image(self, data):
+        transform = transforms.Compose(
+            [
+                transforms.Resize(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
         )
-    except:
-        return jsonify({"error": "Failed"}), 400
 
+        MODEL = self.fetch_model()
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+        imagenet_labels = requests.get(self.LABELS_URL).json()
+
+        image = Image.open(data)
+
+        output = MODEL(transform(image).unsqueeze(0))
+        prediction = torch.argmax(output).item()
+        confidence = torch.softmax(output, 1).max().item()
+
+        return {
+            "class_id": prediction,
+            "class_name": imagenet_labels[prediction],
+            "confidence": round(confidence * 100, 1),
+        }
